d5d8e727f42cd297f0c731b9b65b7484ff4254c7
Tribanocci <kostakitsan@gmail.com>
2023-08-14 21:11:22 +0300

    second

---
+++ Portofolio/myscript.js
@@ -0,0 +1,18 @@
+
+
+var x = window.matchMedia("(max-width: 991px)")
+var is_hidden = true;
+
+function mymobnav(){
+    if (x.matches) { // If media query matches
+        if (is_hidden){
+            document.getElementById('header').style.left = '0';
+            is_hidden = false;
+        }
+        else{
+            document.getElementById('header').style.left = '-300px';
+            is_hidden = true;
+        }
+    }
+    
+}
