a0d3e3133741ec1377792c7d83d74fa56d7ba3db
Tribanocci <kostakitsan@gmail.com>
2023-08-29 16:35:00 +0300

    Update myscript.js

--- Portofolio/myscript.js
+++ Portofolio/myscript.js
@@ -19,3 +19,10 @@ function mymobnav(){
     }
     
 }
+
+$(document).ready(function () {
+    $('.nav-link').click(function () {
+        $('.nav-link').removeClass('active');
+        $(this).addClass('active');
+    });
+});
