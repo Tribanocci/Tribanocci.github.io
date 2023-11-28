9b8bc7392b01ab01ae2b66ada6383b9c5247151f
Tribanocci <kostakitsan@gmail.com>
2023-11-29 01:29:06 +0200

    Update myscript.js

--- Portfolio-CAD/myscript.js
+++ Portfolio-CAD/myscript.js
@@ -78,5 +78,12 @@ $(document).ready(function () {
   });
 
 
-  const queryString = window.location.search;
+const queryString = window.location.search;
 console.log('test', queryString);
+const urlParams = new URLSearchParams(queryString);
+
+$(document).ready(function () {
+  if(urlParams.has('pdf')){
+    $('#lightboxModal4').modal('show');
+  }
+});
